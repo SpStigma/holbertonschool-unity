746d30c43460ce60da0af8a8656c2987edd51f80
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-29 15:44:38 +0200

    Add: Git clone.

---
+++ unity_publishing/Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+
+	void Update () 
+	{
+		transform.Rotate(45 * Time.deltaTime, 0, 0);
+	}
+}
