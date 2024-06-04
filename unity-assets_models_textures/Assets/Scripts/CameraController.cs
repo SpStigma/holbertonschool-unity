93a4b6f5b0ec597fa9f17865909a6b51ae5ca9b4
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-06-04 02:10:49 +0200

    Add: Camera Controller to move the camera around an object with held mousebutton.

---
+++ unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float _mouseSensitivity = 3.0f;
+
+    private float _rotationY;
+    private float _rotationX;
+
+    
+    public Transform target;
+
+    private float distanceFromTarget = 6.25f;
+    private Vector3 _currentRotation;
+    private Vector3 _smoothVelocity = Vector3.zero;
+    public float smoothTime = 0.2f;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity;
+            float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity;
+            
+            _rotationY += mouseX;
+            _rotationX -= mouseY;
+
+            Vector3 nextRotation = new Vector3(_rotationX, _rotationY, 0);
+            _currentRotation = Vector3.SmoothDamp(_currentRotation, nextRotation, ref _smoothVelocity, smoothTime);
+
+            transform.localEulerAngles = _currentRotation;
+        }
+        transform.position = target.position - transform.forward * distanceFromTarget;
+    }
+}
