ef997e5e4b5b6b44ddb04b050dc3b64a44d719a7
Rivet Yoann <GitHubStigma@gmail.com>
2024-06-18 16:03:19 +0200

    ADD Script to navigate to levels and options and quitgame.

---
+++ unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Button backButton;
+
+    // Variable to store the name of the previous scene
+    private string previousScene;
+
+    void Start()
+    {
+        // Get the previous scene from PlayerPrefs
+        previousScene = PlayerPrefs.GetString("PreviousScene", "MainMenu"); // Default to "MainMenu" if not set
+
+        // Add listener to the back button
+        backButton.onClick.AddListener(Back);
+    }
+
+    public void Back()
+    {
+        // Load the previous scene
+        SceneManager.LoadScene(previousScene);
+    }
+}
