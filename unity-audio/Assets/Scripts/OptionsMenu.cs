1653b7a656c78fabdaa23563e3d8d0d7c1db2e15
Rivet Yoann <GitHubStigma@gmail.com>
2024-07-30 17:13:44 +0200

    Add slider and audio mixer reaction.

--- unity-audio/Assets/Scripts/OptionsMenu.cs
+++ unity-audio/Assets/Scripts/OptionsMenu.cs
@@ -1,11 +1,26 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public Toggle isYInverted;
+    public Slider BGMSlider;
+    public AudioMixer master;
 
+    public void Start()
+    {
+        float currentVolume;
+        if (master.GetFloat("BGM", out currentVolume))
+        {
+            BGMSlider.value = Mathf.Pow(10, currentVolume / 20);
+        }
+        else
+        {
+            BGMSlider.value = 1f;
+        }
+    }
     public void Back()
     {
         SceneController.instance.LoadPreviousScene();
@@ -22,6 +37,18 @@ public class OptionsMenu : MonoBehaviour
         {
             GameSettings.isInverted = false;
             SceneController.instance.LoadPreviousScene();
+        }
+    }
+
+    public void SetSoundBGM(float slider)
+    {
+        if (slider <= 0.001f)
+        {
+            master.SetFloat("BGM", -80f);
+        }
+        else
+        {
+            master.SetFloat("BGM", Mathf.Log10(slider) * 20);
         }
     }
 }
