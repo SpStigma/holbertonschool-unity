164b533b5ada0de61386c6e6ed6b935d528114af
SpStigma <githubstigma@gmail.com>
2024-09-30 02:13:01 +0200

    Start to create a interaction to chose one plane and disable rendermesh and activate one button.

---
+++ unity-ar_slingshot_game/Assets/Scripts/ChosePlanes.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+
+public class ChosePlanes : MonoBehaviour
+{
+    public ARRaycastManager raycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    public static ARPlane selectedPlane;
+    public GameObject button;
+
+    void Update()
+    {
+        /*
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinBounds))
+                {
+                    // Get the selected plane
+                    ARPlane plane = hits[0].trackable as ARPlane;
+                    if (plane != null)
+                    {
+                        SelectPlane(plane);
+                    }
+                }
+            }
+        }
+        */
+        if (Input.GetMouseButtonDown(0))  // Mouse left click simulates touch
+        {
+            Vector2 screenPosition = Input.mousePosition;
+
+            // Raycast against detected planes
+            if (raycastManager.Raycast(screenPosition, hits, TrackableType.PlaneWithinBounds))
+            {
+                ARPlane hitPlane = hits[0].trackable as ARPlane;
+
+                // Select the plane if it's valid
+                if (hitPlane != null)
+                {
+                    Debug.Log("plane selected");
+                    SelectPlane(hitPlane);
+                }
+            }
+        }
+
+    }
+
+    public void SelectPlane(ARPlane plane)
+    {
+        selectedPlane = plane;
+        DisablePlanes();
+        button.SetActive(true);
+
+    }
+
+    public void DisablePlanes()
+    {
+        ARPlaneManager planeManager =  GetComponent<ARPlaneManager>();
+        foreach(var plane in planeManager.trackables)
+        {
+            if(plane != ChosePlanes.selectedPlane)
+            {
+                plane.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+    }
+}
